2010f4b6adb1c5f72a476411db01ae9275d7bcbe
Sierra Brown <sierrarazgriz@gmail.com>
2016-04-08 12:20:18 -0700

    Controller, Model added
    Added General Notice controller and model

---
+++ app/Http/Controllers/CCIA/GeneralNoticeController.php
@@ -0,0 +1,115 @@
+<?php
+    
+    /**
+     * Copyright (c) 2016 "Sierra Brown"
+     *
+     * This file is part of Aurorastation-Wi
+     * Aurorastation-Wi is free software: you can redistribute it and/or modify
+     * it under the terms of the GNU Affero General Public License as
+     * published by the Free Software Foundation, either version 3 of the
+     * License, or (at your option) any later version.
+     *
+     * This program is distributed in the hope that it will be useful,
+     * but WITHOUT ANY WARRANTY; without even the implied warranty of
+     * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+     * GNU Affero General Public License for more details.
+     *
+     * You should have received a copy of the GNU Affero General Public License
+     * along with this program. If not, see <http://www.gnu.org/licenses/>.
+     *
+     */
+    
+    namespace App\Http\Controllers\CCIA;
+    
+    use Illuminate\Http\Request;
+    use App\Http\Requests;
+    use App\Http\Controllers\Controller;
+    use Yajra\Datatables\Datatables;
+    
+    class GeneralNoticeController extends Controller
+    {
+        public function __construct(Request $request)
+        {
+            
+        }
+        
+        public function index()
+        {
+            return view('ccia.generalnotice.index');
+        }
+        
+        public function getEdit(Request $request)
+        {
+            if ($request->user()
+                        ->cannot('ccia_general_notices_edit')
+            ) {
+                abort('403', 'You do not have permission to edit CCIA General Notices.');
+            }
+            
+            // TODO Code that processes getEdit
+            
+            return redirect()->route('ccia.generalnotice.index');
+        }
+        
+        public function postEdit(Request $request)
+        {
+            if ($request->user()
+                        ->cannot('ccia_general_notices_edit')
+            ) {
+                abort('403', 'You do not have permission to edit CCIA General Notices.');
+            }
+            
+            // TODO Code that processes postEdit
+            
+            return redirect()->route('ccia.generalnotice.index');
+        }
+        
+        public function getAdd(Request $request)
+        {
+            if ($request->user()
+                        ->cannot('ccia_general_notices_edit')
+            ) {
+                abort('403', 'You do not have permission to edit CCIA General Notices.');
+            }
+            
+            // TODO Code that processes getAdd
+            
+            return redirect()->route('ccia.generalnotice.index');
+        }
+        
+        public function postAdd(Request $request)
+        {
+            if ($request->user()
+                        ->cannot('ccia_general_notices_edit')
+            ) {
+                abort('403', 'You do not have permission to edit CCIA General Notices.');
+            }
+            
+            // TODO Code that processes postAdd
+            
+            return redirect()->route('ccia.generalnotice.index');
+        }
+        
+        public function delete(Request $request)
+        {
+            if ($request->user()
+                        ->cannot('ccia_general_notices_edit')
+            ) {
+                abort('403', 'You do not have permission to edit CCIA General Notices.');
+            }
+            
+            // TODO Code that processes delete
+            
+            return redirect()->route('ccia.generalnotice.index');
+        }
+        
+        public function getData(Request $request)
+        {
+            $data = GeneralNotice::select(['id', 'title', 'message']);
+            
+            return Datatables::of($data)
+                             ->editColumn('title', '<a href="{{route{\'ccia.generalnotices.edit.get\', [\'id\' -> $id)}}">{{$name}}</a>')
+                             ->make();
+            
+        }
+    }
