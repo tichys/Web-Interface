680d8c302b8f840d44a38c877ffb70fd92788e4c
Sierra Brown <sierrarazgriz@gmail.com>
2016-04-10 11:21:44 -0700

    INCOMPLETE - index.blade.php

---
+++ resources/views/ccia/generalnotice/index.blade.php
@@ -0,0 +1,61 @@
+{{--Copyright (c) 2016 "Sierra Brown"--}}
+
+{{--This file is part of the Aurora Webinterface--}}
+
+{{--The Aurora Webinterface is free software: you can redistribute it and/or modify--}}
+{{--it under the terms of the GNU Affero General Public License as--}}
+{{--published by the Free Software Foundation, either version 3 of the--}}
+{{--License, or (at your option) any later version.--}}
+
+{{--This program is distributed in the hope that it will be useful,--}}
+{{--but WITHOUT ANY WARRANTY; without even the implied warranty of--}}
+{{--MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the--}}
+{{--GNU Affero General Public License for more details.--}}
+
+{{--You should have received a copy of the GNU Affero General Public License--}}
+{{--along with this program. If not, see <http://www.gnu.org/licenses/>.<!DOCTYPE html>--}}
+
+@extends('layouts.app')
+
+@section('styles')
+    <link href="{{ asset('/assets/css/datatables.bootstrap.css') }}" rel="stylesheet">
+@endsection
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+                <div class="panel panel-default">
+                    <div class="panel-heading">CCIA General Notices Overview</div>
+
+                    <div class="panel-body">
+                        @can('ccia_general_notices_edit')<p><a href="{{route('ccia.generalnotice.add.post')}}" class="btn btn-success" role="button">Add new Notice</a></p>@endcan()
+                        <table id="general-notices-table" class="table table-condensed">
+                            <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Title</th>
+                                <th>Message</th>
+                            </tr>
+                            </thead>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
+@section('javascripts')
+    <script src="{{ asset('/assets/js/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('/assets/js/datatables.bootstrap.js') }}"></script>
+    <script>
+        $(function() {
+            $('#general-notices-table').DataTable({
+                processing: true,
+                serverSide: true,
+                ajax: '{{ route('ccia.generalnotice.data') }}'
+            });
+        });
+    </script>
+@endsection
