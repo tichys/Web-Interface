31aa5de7923924d944c486b3d2bbc44c0c2b854a
Werner <1331699+Arrow768@users.noreply.github.com>
2018-03-27 22:17:01 +0200

    Bump Version Number to 0.20.0

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -237,7 +237,7 @@
 </body>
 <footer>
     <div class="footer navbar-fixed-bottom">
-        <small><p class="text-muted">Aurora Webinterface &copy; 2016-2018 by Werner Maisl - Licensed under the AGPL - Version 0.19.1</p></small>
+        <small><p class="text-muted">Aurora Webinterface &copy; 2016-2018 by Werner Maisl - Licensed under the AGPL - Version 0.20.0</p></small>
     </div>
 </footer>
 </html>
