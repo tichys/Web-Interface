b26b525bb96fb676d9a53c03e156e7ecd382fe70
Werner <1331699+Arrow768@users.noreply.github.com>
2018-05-21 01:08:28 +0200

    Fix wrong endtime being displayed at the poll. Fix missing poll link

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -243,7 +243,7 @@
 </body>
 <footer>
     <div class="footer navbar-fixed-bottom">
-        <small><p class="text-muted">Aurora Webinterface &copy; 2016-2018 by Werner Maisl - Licensed under the AGPL - Version 0.24.2</p></small>
+        <small><p class="text-muted">Aurora Webinterface &copy; 2016-2018 by Werner Maisl - Licensed under the AGPL - Version 0.24.3</p></small>
     </div>
 </footer>
 </html>
